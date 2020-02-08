7104c45735ff35dc79940e8adfddbd9b3ed14482
Kayo Renato <kayorenatocontato@gmail.com>
2020-02-08 12:43:50 -0300

    Created Page to Instructor Create

--- routes.js
+++ routes.js
@@ -1,12 +1,28 @@
 const express = require('express')
 const routes = express.Router()
 
+
 routes.get('/', (req, res) => {
   return res.redirect("/instructors")
 })
 
 routes.get('/instructors', (req, res) => {
   return res.render("instructors/index")
+})
+
+routes.get('/instructors/create', (req, res) => {
+  return res.render("instructors/create")
+})
+
+routes.post('/instructors', (req, res) => {
+  const keys =  Object.keys(req.body)
+  // // Validação de dados pós submit - Pessímo para a Experiência do Usuário
+  // for(key of keys){
+  //   if(req.body[key] == ''){
+  //     return res.send(`Preencha todos os campos! ${key}`)
+  //   }
+  // }
+  return res.send(req.body)
 })
 
 routes.get('/members', (req, res) => {
